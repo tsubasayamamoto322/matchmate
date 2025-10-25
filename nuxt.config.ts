8d08e07f8cb6aae35d73f104c3b9f0676c9d6e3a
ryousuke.mizokami <ming10r1170@gmail.com>
2025-10-25 18:22:42 +0900

    supabase接続

---
+++ nuxt.config.ts
@@ -0,0 +1,25 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/image',
+    '@nuxt/test-utils',
+    '@nuxt/ui',
+    '@nuxtjs/supabase',
+  ],
+  runtimeConfig: {
+    // サーバーサイドでのみ利用するシークレット（例: サービスロールキーなど）
+
+    public: {
+      // フロントエンドにも公開する情報
+      // Supabaseモジュールが探す構造でURLとKeyを設定します。
+      supabase: {
+        url: process.env.SUPABASE_URL,
+        key: process.env.SUPABASE_ANON_KEY,
+      }
+    }
+  },
+})
