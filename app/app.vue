74d7355ec7207b0c336b4412e0a4c79098dbf7e6
ryousukemizokami <130983214+ryousukemizokami@users.noreply.github.com>
2025-11-02 16:45:55 +0900

    Feature/hoge (#12)
    * コンフリ解消
    
    * package.jsonコンフリ解消
    
    * Nuxt4ディレクトリ整理

--- app/app.vue
+++ app/app.vue
@@ -1,4 +1,15 @@
 <script setup>
+import { createClient } from '@supabase/supabase-js'
+const config = useRuntimeConfig()
+const supabase = createClient(config.public.supabaseUrl, config.public.supabaseKey)
+const instruments = ref([])
+async function getInstruments() {
+  const { data } = await supabase.from('instruments').select()
+  instruments.value = data
+}
+onMounted(() => {
+  getInstruments()
+})
 </script>
 <template>
   <div>
