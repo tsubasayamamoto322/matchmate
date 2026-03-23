737a0b7d2d41913ea66f7b60f03804adc8452d74
ryousuke.mizokami <ming10r1170@gmail.com>
2026-03-23 11:46:42 +0900

    タブのアイコン設定

--- app/app.vue
+++ app/app.vue
@@ -1,4 +1,13 @@
-<script setup>
+<script setup lang="ts">
+import logo from '~/assets/images/logo.png'
+
+useHead({
+  link: [
+    { rel: 'icon', type: 'image/png', href: logo },
+    { rel: 'shortcut icon', type: 'image/png', href: logo },
+    { rel: 'apple-touch-icon', href: logo },
+  ],
+})
 </script>
 <template>
   <div>
